4b9a6a035fbd28969dc16dc50ef6f4d5618560c0
rkarimgit <raed.karim@sheridancollege.ca>
2020-01-20 23:33:27 -0500

    Revert ""
    This reverts commit d24460280fbfa939fa495240fa3bd1dd0cf76efb.

--- src/javaapplication8/JavaApplication8.java
+++ src/javaapplication8/JavaApplication8.java
@@ -12,7 +12,6 @@ class NewMain {
     public static void main(String[] args) {
         System.out.println("my greeting!");
         System.out.println("now I added this!");
-        System.out.println("now I changed this and I try to revert!");
     }
 
 }
